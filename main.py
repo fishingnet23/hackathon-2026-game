176ca0d4399233539cbe61e9e3f3d094d25c2641
fishingnet23 <netanel.gadasi@gmail.com>
2025-03-09 17:20:46 +0200

    smart_blit v1
    function for positioning v1

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 
 pygame.init()
 
-
 # idea notes
 # add function to convert a number from 0 to 1 into a position on screen (0 = 0, 1 = WIDTH)
 
@@ -22,10 +21,16 @@ FPS = 60
 main_clock = pygame.time.Clock()
 
 
-def load_image_with_scale(path, factors: list):
+def load_image_with_scale(path, factors: list, dimesions=(1, 1)):
+    global WIDTH, HEIGHT
     image = pygame.image.load(path)
-    image = pygame.transform.scale(image, (image.get_width() * factors[0], image.get_height() * factors[1]))
+    image = pygame.transform.scale(image,
+                                   (base_width * dimesions[0] * factors[0], base_height * dimesions[1] * factors[1]))
     return image
+
+
+def smart_blit(surf, image, xScale, yScale, center_x=False, center_y=False):
+        surf.blit(image, (WIDTH * xScale - image.get_width() / 2, HEIGHT * yScale - image.get_height() / 2))
 
 
 
@@ -40,9 +45,9 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    img = load_image_with_scale("data_folder/test_img/bg.jpg", SCALE_FACTOR_LIST)
+    img = load_image_with_scale("data_folder/test_img/bg.jpg", SCALE_FACTOR_LIST, (0.8, 0.5))
 
-    screen.blit(img, (WIDTH / 2 - img.get_width() / 2, HEIGHT / 2 - img.get_height() / 2))
+    smart_blit(screen, img, 0.5, 0.5, False)
 
     main_clock.tick(FPS)
     pygame.display.flip()
