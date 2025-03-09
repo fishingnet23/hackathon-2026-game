b7b282bf464300dd4345b43c2ee2057f87a0ebe5
fishingnet23 <157644345+fishingnet23@users.noreply.github.com>
2025-03-09 12:18:12 +0200

    notes

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import pygame
 
 pygame.init()
 
+
+# idea notes
+# add function to convert a number from 0 to 1 into a position on screen (0 = 0, 1 = WIDTH)
+
+
 base_width = 1920
 base_height = 1080
 
-WIDTH = 1600
-HEIGHT = 800
+WIDTH = 1920 * 0.6
+HEIGHT = 1080 * 0.6
 
 SCALE_FACTOR_LIST = [WIDTH / base_width, HEIGHT / base_height]
 
@@ -21,6 +26,7 @@ def load_image_with_scale(path, factors: list):
     image = pygame.image.load(path)
     image = pygame.transform.scale(image, (image.get_width() * factors[0], image.get_height() * factors[1]))
     return image
+
 
 
 while run:
